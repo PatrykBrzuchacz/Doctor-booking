7f806e474c057de11d4d3f288bed6ea0bb6a8c58
PatrykBrzuchacz <patrykbrzuchacz@gmail.com>
2019-02-09 21:02:51 +0100

    Connect clinicbranch with a doctor

--- src/main/java/backend/doctorbooking/booking/controller/DoctorController.java
+++ src/main/java/backend/doctorbooking/booking/controller/DoctorController.java
@@ -1,19 +1,28 @@
 package backend.doctorbooking.booking.controller;
 
 import backend.doctorbooking.booking.model.Doctor;
+import backend.doctorbooking.booking.service.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RepositoryRestController
+@RequestMapping("/api/clinics")
 public class DoctorController {
 
-    @PreAuthorize("hasRole('ROLE_DOCTOR')")
-    @PutMapping("/api/clinics/{id}/branchs/{id2}")
-    public ResponseEntity<Doctor> addDoctorToBranch(){
+    @Autowired
+    private DoctorService doctorService;
 
+    @PreAuthorize("hasRole('ROLE_DOCTOR')")
+    @PutMapping("/{id}/branches/{id2}/addDoctor")
+    public ResponseEntity<?> addDoctorToBranch(@PathVariable("id2") Long idBranch){
+        doctorService.addClinicBranchToDoctor(idBranch);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/backend/doctorbooking/booking/service/DoctorService.java
+++ src/main/java/backend/doctorbooking/booking/service/DoctorService.java
@@ -1,8 +1,11 @@
 package backend.doctorbooking.booking.service;
 
+import backend.doctorbooking.booking.model.Doctor;
 import backend.doctorbooking.booking.model.clinic.ClinicBranch;
 import backend.doctorbooking.booking.repository.DoctorRepository;
 import backend.doctorbooking.booking.repository.clinic.ClinicBranchRepository;
+import backend.doctorbooking.common.security.model.User;
+import backend.doctorbooking.common.service.UserHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +16,17 @@ public class DoctorService {
     private DoctorRepository doctorRepository;
 
     @Autowired
+    private UserHelper userHelper;
+
+    @Autowired
     private ClinicBranchRepository clinicBranchRepository;
 
+    public Doctor addClinicBranchToDoctor(Long id){
+        User loggedUser = userHelper.getLoggedUser();
+        Doctor doctor = doctorRepository.findDoctorByUserDoctor(loggedUser);
+        doctor.setClinicBranch(clinicBranchRepository.findClinicBranchById(id));
+        return doctorRepository.save(doctor);
+    }
 
 
 }
